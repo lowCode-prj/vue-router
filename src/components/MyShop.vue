7f3b1eadbafb13cf060393be63fa996e7dab8887
zhangweijie_zz <1715702588@qq.com>
2022-09-08 11:05:03 +0800

    $router.push使用

--- src/components/MyShop.vue
+++ src/components/MyShop.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
     <h1>我是商城</h1>
+    <button @click="linkToMyhome">linkToMyhome</button>
   </div>
 </template>
 
 <script>
 export default {
-    name:'MyShop'
+    name:'MyShop',
+    methods:{ 
+      linkToMyhome(){
+        this.$router.push('/MyHome')
+      }
+    }
 }
 </script>
 
 <style>
 
 </style>
+
